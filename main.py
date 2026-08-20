e25a456b99ce16cb6552e9065006e897f4ab7e83
ercanayin1973-jpg <ercanayin1973@gmail.com>
2026-08-20 13:10:31 +0100

    Update PDF report filename and content details

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 MY_EMAIL = os.environ.get("MY_EMAIL")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
-def create_pdf_report(ai_text, kanton, status, filename="Steuererklaerung_Offiziell.pdf"):
+def create_pdf_report(ai_text, kanton, status, filename="Steuererklaerung_Offiziell_ZH.pdf"):
     doc = SimpleDocTemplate(filename, pagesize=A4, rightMargin=20, leftMargin=20, topMargin=20, bottomMargin=20)
     elements = []
     styles = getSampleStyleSheet()
@@ -50,10 +50,10 @@ def create_pdf_report(ai_text, kanton, status, filename="Steuererklaerung_Offizi
         textColor=colors.whitesmoke
     )
 
-    # 1. Kanton ve Kişisel Bilgiler
+    # 1. Resmi Form Başlığı ve Kişisel Bilgiler (Eş ve Çocuklar Dahil)
     header_data = [
-        [Paragraph(f"<b>OFFIZIELLE STEUERERKLÄRUNG – KANTON {kanton.upper()}</b>", form_title)],
-        [Paragraph(f"<b>Steuerpflichtige(r):</b> Max & Erika Muster | <b>Adresse:</b> Bahnhofstrasse 10, 8001 {kanton}<br/><b>AHV-Nr:</b> 756.1234.5678.90 | <b>Zivilstand:</b> {status} | <b>Steuerjahr:</b> 2026", meta_style)]
+        [Paragraph(f"<b>STEUERERKLÄRUNG 2025 / 2026 – KANTON {kanton.upper()}</b>", form_title)],
+        [Paragraph(f"<b>Person 1:</b> Max Muster (Geb. 15.05.1985) | <b>Person 2 (Ehepartner):</b> Erika Muster (Geb. 20.08.1988)<br/><b>Adresse:</b> Bahnhofstrasse 10, 8001 {kanton} | <b>Zivilstand:</b> {status} | <b>Kinder:</b> 1 Kind (Noah Muster, Geb. 10.02.2018)", meta_style)]
     ]
     header_table = Table(header_data, colWidths=[550])
     header_table.setStyle(TableStyle([
@@ -67,13 +67,13 @@ def create_pdf_report(ai_text, kanton, status, filename="Steuererklaerung_Offizi
     elements.append(header_table)
     elements.append(Spacer(1, 6))
 
-    # 2. Gelirler Tablosu (Brutto ve Sosyal Kesintiler)
+    # 2. Gelirler Tablosu (Lohnausweis Person 1 & Person 2)
     income_data = [
-        [Paragraph("<b>Code</b>", header_style), Paragraph("<b>Einkommensart / Beschreibung (Lohnausweis Standard)</b>", header_style), Paragraph("<b>Betrag (CHF)</b>", header_style)],
-        [Paragraph("Ziff. 1.1", cell_style), Paragraph("Bruttolohn (Hauptberuf - gemäss Lohnausweis)", cell_style), Paragraph("125'000 CHF", cell_style)],
-        [Paragraph("Abzug", cell_style), Paragraph("AHV / IV / EO & Pensionskasse (Obligat. Sozialabzüge)", cell_style), Paragraph("- 15'750 CHF", cell_style)],
-        [Paragraph("Ziff. 1.2", cell_style), Paragraph("Nebeneinkünfte / Erwerbsersatz", cell_style), Paragraph("10'000 CHF", cell_style)],
-        [Paragraph("Ziff. 3.1", cell_style), Paragraph("Wertschriften & Bankkonto Zinserträge", cell_style), Paragraph("450 CHF", cell_style)]
+        [Paragraph("<b>Ziffer</b>", header_style), Paragraph("<b>Einkommensart / Beschreibung (Lohnausweis / Belege)</b>", header_style), Paragraph("<b>Betrag (CHF)</b>", header_style)],
+        [Paragraph("Ziff. 1.1", cell_style), Paragraph("Bruttolohn Person 1 (Hauptberuf - Arbeitgeber AG)", cell_style), Paragraph("125'000 CHF", cell_style)],
+        [Paragraph("Ziff. 1.2", cell_style), Paragraph("Bruttolohn Person 2 / Ehepartner (Teilzeit)", cell_style), Paragraph("45'000 CHF", cell_style)],
+        [Paragraph("Abzug", cell_style), Paragraph("AHV / IV / EO & Pensionskasse (Oblig. Sozialabzüge beider)", cell_style), Paragraph("- 18'700 CHF", cell_style)],
+        [Paragraph("Ziff. 3.1", cell_style), Paragraph("Wertschriften & Bankkonto Zinserträge (Wertschriftenverzeichnis)", cell_style), Paragraph("450 CHF", cell_style)]
     ]
     t_income = Table(income_data, colWidths=[50, 400, 100])
     t_income.setStyle(TableStyle([
@@ -83,19 +83,19 @@ def create_pdf_report(ai_text, kanton, status, filename="Steuererklaerung_Offizi
         ('TOPPADDING', (0, 0), (-1, -1), 3),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 3),
     ]))
-    elements.append(Paragraph("<b>1. EINKOMMEN & SOZIALE ABZÜGE (BRUTTO-NETTO-UEBERGANG)</b>", meta_style))
+    elements.append(Paragraph("<b>1. EINKOMMEN & SOZIALE ABZÜGE (LOHNAUSWEISE BEIDER EHEPARTNER)</b>", meta_style))
     elements.append(Spacer(1, 2))
     elements.append(t_income)
     elements.append(Spacer(1, 6))
 
-    # 3. İndirimler Tablosu (Benzin/Yol Giderleri - Fahrkosten & Benzin Dahil)
+    # 3. İndirimler Tablosu (Berufsauslagen, 3a, Krankenkasse, Krankheitskosten)
     deduction_data = [
-        [Paragraph("<b>Code</b>", header_style), Paragraph("<b>Abzugskategorie (Zulässige Abzüge)</b>", header_style), Paragraph("<b>Details / Gesetzliche Grundlage</b>", header_style), Paragraph("<b>Betrag (CHF)</b>", header_style)],
-        [Paragraph("Ziff. 2.1", cell_style), Paragraph("Berufsauslagen (Fahrkosten)", cell_style), Paragraph("Fahrkosten Arbeitsweg (Auto/Benzin oder ÖV)", cell_style), Paragraph("3'200 CHF", cell_style)],
-        [Paragraph("Ziff. 2.2", cell_style), Paragraph("Säule 3a", cell_style), Paragraph("Private Vorsorge (Maximalbetrag)", cell_style), Paragraph("7'056 CHF", cell_style)],
-        [Paragraph("Ziff. 2.3", cell_style), Paragraph("Krankenkasse", cell_style), Paragraph("Krankenkassenprämien (Grund-/Zusatzvers.)", cell_style), Paragraph("6'800 CHF", cell_style)],
-        [Paragraph("Ziff. 2.4", cell_style), Paragraph("Krankheitskosten", cell_style), Paragraph("Selbstbehalt Arzt, Zahnarzt (Eigenleistung)", cell_style), Paragraph("1'200 CHF", cell_style)],
-        [Paragraph("Ziff. 4.0", cell_style), Paragraph("Spenden", cell_style), Paragraph("Zuwendungen an steuerbefreite Org.", cell_style), Paragraph("500 CHF", cell_style)]
+        [Paragraph("<b>Ziffer</b>", header_style), Paragraph("<b>Abzugskategorie (Zulässige Abzüge)</b>", header_style), Paragraph("<b>Details / Gesetzliche Grundlage</b>", header_style), Paragraph("<b>Betrag (CHF)</b>", header_style)],
+        [Paragraph("Ziff. 2.1", cell_style), Paragraph("Berufsauslagen", cell_style), Paragraph("Fahrkosten Arbeitsweg (ÖV/Auto) & Verpflegung", cell_style), Paragraph("4'500 CHF", cell_style)],
+        [Paragraph("Ziff. 2.2", cell_style), Paragraph("Säule 3a", cell_style), Paragraph("Private Vorsorge (Maximalbetrag 2025/2026)", cell_style), Paragraph("14'512 CHF", cell_style)],
+        [Paragraph("Ziff. 2.3", cell_style), Paragraph("Krankenkasse", cell_style), Paragraph("Krankenkassenprämien (Kantonaler Maximalabzug)", cell_style), Paragraph("5'800 CHF", cell_style)],
+        [Paragraph("Ziff. 2.4", cell_style), Paragraph("Krankheitskosten", cell_style), Paragraph("Selbstbehalt Arzt & Zahnarzt (Nachweis erforderlich)", cell_style), Paragraph("1'200 CHF", cell_style)],
+        [Paragraph("Ziff. 4.0", cell_style), Paragraph("Spenden", cell_style), Paragraph("Zuwendungen an steuerbefreite Org. (max 20% Netto)", cell_style), Paragraph("500 CHF", cell_style)]
     ]
     t_deduction = Table(deduction_data, colWidths=[50, 110, 290, 100])
     t_deduction.setStyle(TableStyle([
@@ -105,17 +105,17 @@ def create_pdf_report(ai_text, kanton, status, filename="Steuererklaerung_Offizi
         ('TOPPADDING', (0, 0), (-1, -1), 3),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 3),
     ]))
-    elements.append(Paragraph("<b>2. ABZÜGE (FAHRKOSTEN/BENZIN, SÄULE 3A, KRANKENKASSE, SPENDEN)</b>", meta_style))
+    elements.append(Paragraph("<b>2. ABZÜGE (BERUF, SÄULE 3A, KRANKENKASSE, GESUNDHEIT, SPENDEN)</b>", meta_style))
     elements.append(Spacer(1, 2))
     elements.append(t_deduction)
     elements.append(Spacer(1, 6))
 
-    # 4. Servet ve Borçlar Tablosu
+    # 4. Wertschriften & Schulden (Wertschriftenverzeichnis & Schuldenverzeichnis)
     asset_data = [
-        [Paragraph("<b>Kategorie</b>", header_style), Paragraph("<b>Beschreibung / Details (Banken, Aktien, Kredite)</b>", header_style), Paragraph("<b>Wert / Betrag (CHF)</b>", header_style)],
-        [Paragraph("Wertschriften", cell_style), Paragraph("Bankguthaben (Kantonalbank & PostFinance)", cell_style), Paragraph("45'000 CHF", cell_style)],
-        [Paragraph("Wertschriften", cell_style), Paragraph("Aktien / Fonds (Schweizer Börse)", cell_style), Paragraph("12'500 CHF", cell_style)],
-        [Paragraph("Schulden", cell_style), Paragraph("Privatkredit / Kreditkartenschulden", cell_style), Paragraph("- 5'000 CHF", cell_style)]
+        [Paragraph("<b>Kategorie</b>", header_style), Paragraph("<b>Beschreibung / Details (Banken, Wertschriften, Kredite)</b>", header_style), Paragraph("<b>Wert / Betrag (CHF)</b>", header_style)],
+        [Paragraph("Wertschriften", cell_style), Paragraph("Bankkonten (Zürcher Kantonalbank & PostFinance)", cell_style), Paragraph("45'000 CHF", cell_style)],
+        [Paragraph("Wertschriften", cell_style), Paragraph("Aktien & Anlagefonds (Depotverzeichnis)", cell_style), Paragraph("12'500 CHF", cell_style)],
+        [Paragraph("Schulden", cell_style), Paragraph("Privatkredit / Schuldenverzeichnis (inkl. Zinsnachweis)", cell_style), Paragraph("- 5'000 CHF", cell_style)]
     ]
     t_asset = Table(asset_data, colWidths=[100, 350, 100])
     t_asset.setStyle(TableStyle([
@@ -125,17 +125,17 @@ def create_pdf_report(ai_text, kanton, status, filename="Steuererklaerung_Offizi
         ('TOPPADDING', (0, 0), (-1, -1), 3),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 3),
     ]))
-    elements.append(Paragraph("<b>3. VERMÖGEN & SCHULDEN (BANKEN, AKTIEN, KREDITE)</b>", meta_style))
+    elements.append(Paragraph("<b>3. VERMÖGEN & SCHULDEN (WERTSCHRIFTENVERZEICHNIS & SCHULDENVERZEICHNIS)</b>", meta_style))
     elements.append(Spacer(1, 2))
     elements.append(t_asset)
     elements.append(Spacer(1, 6))
 
-    # 5. Vergilendirilebilir Matrah ve Tahmini Vergi Yükü
+    # 5. Vergilendirilebilir Matrah ve Üç Kademeli Vergi Yükü (Staats-, Gemeinde-, Bundessteuer)
     tax_load_data = [
-        [Paragraph(f"<b>Berechnungsgrundlage nach Kantonalem Recht ({kanton})</b>", header_style), Paragraph("<b>Betrag / Schätzung (CHF)</b>", header_style)],
-        [Paragraph("Steuerbares Einkommen (Brutto abzüglich Sozialabgaben & aller Abzüge)", cell_style), Paragraph("approx. 96'350 CHF", cell_style)],
-        [Paragraph("Steuerbares Vermögen (Netto-Vermögen nach Schulden)", cell_style), Paragraph("approx. 52'500 CHF", cell_style)],
-        [Paragraph("<b>Geschätzte Steuerlast (Kanton {kanton}, Gemeinde & Bund)</b>", header_style), Paragraph("<b>approx. 11'900 CHF</b>", cell_style)]
+        [Paragraph(f"<b>Steuerberechnung nach Zürcher Steuerrecht (Kanton {kanton})</b>", header_style), Paragraph("<b>Betrag (CHF)</b>", header_style)],
+        [Paragraph("Steuerbares Einkommen (Nach allen Abzügen & Sozialbeiträgen)", cell_style), Paragraph("130'550 CHF", cell_style)],
+        [Paragraph("Steuerbares Vermögen (Nach Abzug Schulden)", cell_style), Paragraph("52'500 CHF", cell_style)],
+        [Paragraph("<b>1. Staatssteuer (Kanton Zürich) & 2. Gemeindesteuer & 3. Direkte Bundessteuer</b>", header_style), Paragraph("<b>Total ca. 16'400 CHF</b>", header_style)]
     ]
     t_tax = Table(tax_load_data, colWidths=[450, 100])
     t_tax.setStyle(TableStyle([
@@ -145,13 +145,13 @@ def create_pdf_report(ai_text, kanton, status, filename="Steuererklaerung_Offizi
         ('TOPPADDING', (0, 0), (-1, -1), 3),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 3),
     ]))
-    elements.append(Paragraph("<b>4. STEUERBARES EINKOMMEN, VERMÖGEN & GESCHÄTZTE STEUERLAST</b>", meta_style))
+    elements.append(Paragraph("<b>4. STEUERBARES EINKOMMEN, VERMÖGEN & GESCHÄTZTE STEUERLAST (3-TEILIG)</b>", meta_style))
     elements.append(Spacer(1, 2))
     elements.append(t_tax)
     elements.append(Spacer(1, 6))
 
-    # 6. Kantonel Açıklamalar
-    elements.append(Paragraph(f"<b>5. KANTONALE HINWEISE FÜR {kanton.upper()}:</b>", meta_style))
+    # 6. Kantonel Açıklamalar ve Uyarılar
+    elements.append(Paragraph(f"<b>5. OFFIZIELLE HINWEISE & BELEGVOLLSTÄNDIGKEIT ({kanton.upper()}):</b>", meta_style))
     elements.append(Spacer(1, 3))
 
     for line in ai_text.split('\n'):
@@ -163,19 +163,15 @@ def create_pdf_report(ai_text, kanton, status, filename="Steuererklaerung_Offizi
     return filename
 
 def process_tax_documents():
-    # ==========================================
-    # BURADAN KANTONU VE MEDENİ DURUMU SEÇEBİLİRSİN
-    # ==========================================
-    secilen_kanton = "Zuerich"      # Örn: "Zuerich", "Bern", "Zug", "Luzern", "Genève"
-    medeni_durum = "Verheiratet"  # Örn: "Verheiratet" (Evli) veya "Ledig" (Bekar)
+    secilen_kanton = "Zuerich"
+    medeni_durum = "Verheiratet"
     
     client = genai.Client(api_key=GEMINI_API_KEY)
     
     prompt = f"""
     Du bist ein Experte für das Schweizer Steuerrecht im Kanton {secilen_kanton}.
-    Zivilstand: {medeni_durum}.
-    Erstelle eine formelle Bestätigung auf HOCHDEUTSCH (Schweizer Steuerstandard).
-    Erkläre kurz die Besonderheiten der Fahrkosten (Auto/Benzin vs. ÖV) und der kantonalen Regelungen für {secilen_kanton}.
+    Zivilstand: {medeni_durum}. Erstelle eine formelle Bestätigung auf HOCHDEUTSCH (Schweizer Steuerstandard).
+    Bestätige die korrekte Trennung von Person 1 und Person 2, die Berücksichtigung des Kindes, die Begrenzung der Krankenkassenprämien nach kantonalen Vorgaben, sowie die 3-teilige Steuerlast (Staats-, Gemeinde- und direkte Bundessteuer).
     Keine türkische Sprache verwenden, ausschliesslich formelles Schweizer Deutsch.
     """
     
@@ -192,10 +188,9 @@ def send_email_with_pdf():
     msg = MIMEMultipart()
     msg['From'] = MY_EMAIL
     msg['To'] = MY_EMAIL
-    msg['Subject'] = f"Steuererklaerung_{kanton}_{durum}.pdf"
-    msg.attach(MIMEText(f"Im Anhang finden Sie die Steuererklärung für den Kanton {kanton} im PDF-Format.", 'plain', 'utf-8'))
+    msg['Subject'] = f"Offizielle_Steuererklaerung_{kanton}_{durum}.pdf"
+    msg.attach(MIMEText(f"Im Anhang finden Sie die vollständige, offizielle Steuererklärung für den Kanton {kanton} ({durum}).", 'plain', 'utf-8'))
 
-    # Kesin PDF uzantısı ve MIME türü ayarı (noname sorununu çözer)
     with open(pdf_path, "rb") as f:
         part = MIMEBase("application", "pdf", name=f"Steuererklaerung_{kanton}.pdf")
         part.set_payload(f.read())
@@ -209,7 +204,7 @@ def send_email_with_pdf():
         server.login(MY_EMAIL, EMAIL_PASSWORD)
         server.sendmail(MY_EMAIL, MY_EMAIL, msg.as_string())
         server.quit()
-        print(f"Kanton {kanton} vergi formu PDF olarak başarıyla gönderildi!")
+        print("Resmi Zürih formu eksiksiz olarak e-posta ile gönderildi!")
     except Exception as e:
         print(f"Hata: {e}")
 
